96640e220aa524878337a70a2e4648fc5d7429d6
root <root@HOME-PC>
2023-08-27 22:00:37 +0300

    5 commit

--- 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main {
-  wqwet qwet qwet tew etetqet qew qwe while (
-      tqwe typedefqwet qwe typedefqwet qew typedefqew tq wet qew
-          typedefqewqewt) {
-    /* code */
-  }
-}

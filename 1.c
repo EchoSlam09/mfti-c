4e54fa4278b144e6df8db12ed22af5b3716485ed
EchoSlam09 <dafidac6@gmail.com>
2023-08-27 21:53:36 +0300

    4 commit

--- 1.c
+++ 1.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 
-int main { wqwet qwet qwet tew }
+int main {
+  wqwet qwet qwet tew etetqet qew qwe while (
+      tqwe typedefqwet qwe typedefqwet qew typedefqew tq wet qew
+          typedefqewqewt) {
+    /* code */
+  }
+}

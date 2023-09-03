fdd2faaf7951751f1cf1d53224f35c4fe993393d
EchoSlam09 <dafidac6@gmail.com>
2023-09-03 13:22:15 +0300

    3-3-3.c

--- 3/3-3-3.c
+++ 3/3-3-3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  int k_rice, k_veg, amount1, amount2, n;
+  int k_rice, k_veg, money;
   scanf("%d", &k_rice);
   scanf("%d", &k_veg);
-  n = k_rice + k_veg + k_veg;
-  printf("%d", n);
+  money = k_rice + k_veg * 2;
+  printf("%d", money);
   return 0;
 }
